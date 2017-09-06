a554e6fd0096c91ae6970fd84acb640fe8e0b032
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-06 14:33:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,13 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
 //
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+/** looping over list of ids of doctors **/
+for($id = 0; $id <= 2; $id+20)
+	{
+	 $url = ("https://forlap.ristekdikti.go.id/mahasiswa/search/".$id);
+  echo $url;
+  
+  //	$link2 = file_get_html($url);
+   
 ?>
