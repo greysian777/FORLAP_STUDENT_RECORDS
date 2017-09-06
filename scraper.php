1d88e344d12b2f490cbcb45414ce32b0e105f59b
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-06 14:38:36 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,8 +6,11 @@ require 'scraperwiki/simple_html_dom.php';
 for($id = 0; $id <= 2; $id+20)
 	{
 	 $url = ("https://forlap.ristekdikti.go.id/mahasiswa/search/".$id);
-  		echo $url;
+  		if($url != null)
   
+		{
+			echo $url;
+		}
   //	$link2 = file_get_html($url);
 	}
 ?>
