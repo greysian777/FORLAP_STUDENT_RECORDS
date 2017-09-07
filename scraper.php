0dcbf00cb032a77819cc74af4841c90de11042ac
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 15:47:28 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,14 +1,9 @@
 <?
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-//
-/** looping over list of ids of doctors **/
-for($id = 20;$id <= 20; $id+=20)
-	{
-	
+
 	
 	 $url = ("https://forlap.ristekdikti.go.id/prodi/search/20");
   		$link = file_get_html($url) ;
-			echo $link;
-	}
+			
 ?>
