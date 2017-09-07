daaed84f8af436d751a91c951590532ad9a847fe
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 15:54:48 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,6 +4,6 @@ require 'scraperwiki/simple_html_dom.php';
 
 	
 	 $url = ("https://forlap.ristekdikti.go.id/prodi/detail/QjAwRkIwREUtMUVCOC00MEMwLTk1MDctQjQ3NzlGRUM5MzQ5");
-  		$link = file_get_content($url) ;
+  		$link = file_get_html($url) ;
 			echo $link;
 ?>
