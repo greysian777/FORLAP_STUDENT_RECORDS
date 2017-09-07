22223654f3e6a237e47a7bb2274ed764642579b5
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-07 15:48:16 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
 	
-	 $url = ("https://forlap.ristekdikti.go.id/prodi/search/20");
+	 $url = ("https://forlap.ristekdikti.go.id/perguruantinggi/detail/NUFBREYzREYtRDBGOC00QTE3LUJERkQtNjEwOEZDNUNFNDA1");
   		$link = file_get_html($url) ;
-			
+			echo $link;
 ?>
