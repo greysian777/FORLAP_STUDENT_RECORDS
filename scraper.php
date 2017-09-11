c93b210c59e9af0e5e514bf8074d8fbba3353608
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-11 15:36:15 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -18,7 +18,7 @@ function get_data($url) {
 }
   		$link = file_get_html($url) ;
 		foreach($link->find("//[@id='mahasiswa']/table/tbody/tr") as $element){
-		$link = $element->find("./tr[2]/td[3]/a" , 0);
+		$link = $element->find("td[3]/a" , 0);
 			echo $link;
 		}
 
