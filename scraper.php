ec78c5affcf5839d421a9aa3c04cfd2e230c867c
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-11 15:00:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,6 +4,18 @@ require 'scraperwiki/simple_html_dom.php';
 
 	
 	 $url = ("https://forlap.ristekdikti.go.id/prodi/detail/QzZGQjk3NkUtQURCMC00NDBCLTk1NTEtNDY1QkYxMUZENDE2/0");
+
+/* gets the data from a URL */
+function get_data($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
   		$link = file_get_html($url) ;
 			echo $link;
 ?>
