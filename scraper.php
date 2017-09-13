efd7271108f74b541e7a3afe92aa116ada93111a
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-13 15:59:51 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,11 +1,75 @@
 <?
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-$link = 'https://forlap.ristekdikti.go.id/mahasiswa/detail/NUNBNDU1ODgtQjFGNS00NzZCLTkxMjMtN0E1QzNCREFBRkUy/0';
+require "simple_html_dom.php";
+error_reporting(0);
+$links = array("https://forlap.ristekdikti.go.id/prodi/detail/NEE1RjZCOTItRjRBOS00RTQ4LTgzN0ItRjBFMjVGOTRGQkIz");
+for($i = 0; $i < count($links); $i++)
+	{
+			$link = file_get_html($links[$i]);
+			if($link)
+			{
+				foreach($link->find("//[@id='mahasiswa']/table/tbody/tr") as $element)
+					{
+						$totalcountofstudenteachsemester	= $element->find("td[3]/a" , 0)->plaintext;
+						
+						$number = $totalcountofstudenteachsemester / 20;
+						$Pages =(int)$number;
+						$student 	= $element->find("td[3]/a" , 0)->href;
+						
+						for($loop = 0; $loop <= $totalcountofstudenteachsemester; $loop+=20)
+						{
+							
+							$urls =  $student . "/". $loop;
+							if($urls != "/0")
+							{
+							$DAKUMENTPAGE = file_get_html($urls);
+								if($DAKUMENTPAGE)
+								{
+									foreach($DAKUMENTPAGE->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr") as $SARTOUT)
+									{
+										$SerNo = $SARTOUT->find("td", 0)->plaintext;
+										$NIM = $SARTOUT->find("td", 1)->plaintext;
+										$Name = $SARTOUT->find("td" , 2)->plaintext;
+										$Namehref = $SARTOUT->find("td/a" , 0)->href;
+										if($SerNo != null || $SerNo != "")
+										{
+											 $Pagestudent =  file_get_html($Namehref++);
+											 if($Pagestudent)
+											 {
+												 //This is for Page Information.
+												echo $nameofcandidate = $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody" , 0);
+											 }
+																						
+										}
+																			
+										
+										
+										
+										
+										
+										
+									/* echo $SerNo . "=> " . $NIM . "--" . $Name . "--"  . $Namehref;	
+									echo "<br/>";
+									echo "<br/>"; */
+								
+									
+										
+										
+										
+									}
+								}
+							}
+							
+							
+						}
+						
+						
+						
+						
+					}
+			}
+	}
 
-$page = file_get_html($link);
-if($page)
-{
-	echo $table = $page->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table");
-}
+
 ?>
