087275bc8ecb9081309ce65931f01cd379aac4e1
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-13 16:37:26 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,7 +2,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 
-$links = array("https://forlap.ristekdikti.go.id/prodi/detail/NEE1RjZCOTItRjRBOS00RTQ4LTgzN0ItRjBFMjVGOTRGQkIz");
+$links = array("https://forlap.ristekdikti.go.id/prodi/detail/MzM5RDRFRUUtODcwRC00QUJBLUI3REYtODU4REFBRkQ4OTRC");
 for($i = 0; $i < count($links); $i++)
 	{
 			$link = file_get_html($links[$i]);
