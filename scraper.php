52980ea8b2ff7067d74280c51524d2157775c9e4
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-13 17:49:03 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -32,10 +32,10 @@ for($i = 0; $i < count($links); $i++)
 										$Namehref = $SARTOUT->find("td/a" , 0)->href;
 																		 $Pagestudent =  file_get_html($Namehref++);
 									
-									if($Namehref)	
-										
+									if($Namehref  != null)	
+									{
 										$Pagestudent =  file_get_html($Namehref++);
-										if($Pagestudent != null){
+										
 										//This is Details of Students.
 									$info['Nama'] 			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[1]/td[3]",0)->plaintext;
 									$info['Jenis']  		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[2]/td[3]",0)->plaintext;
