d2158daa676518729d1976c9cbc4082d22c5a627
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-14 14:41:37 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,6 +1,7 @@
 <?
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
+require 'simple_html_dom.php';
 $links = array("https://forlap.ristekdikti.go.id/prodi/detail/MzM5RDRFRUUtODcwRC00QUJBLUI3REYtODU4REFBRkQ4OTRC");
 for($i = 0; $i < count($links); $i++)
 	{
