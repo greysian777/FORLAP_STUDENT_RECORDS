42471d9c914e61239d6be35879f472f376d15663
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-14 16:46:44 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -27,8 +27,32 @@ for($i = 0; $i < count($links); $i++)
 										$NIM = $SARTOUT->find("td", 1)->plaintext;
 										$Name = $SARTOUT->find("td" , 2)->plaintext;
 										$Namehref = $SARTOUT->find("td/a" , 0)->href;
-									
-										echo "1 => "  . $Namehref;
+										for($loopo = 1 ; $loopo < sizeof($data); $loopo++)
+										{
+												$URL = $data[$loopo];
+												$Pagestudent = file_get_html($URL);
+											if($Pagestudent)
+											{
+											//This is Details of Students.
+											$Nama 				= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[1]/td[3]",0)->plaintext;
+											$Jenis  			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[2]/td[3]",0)->plaintext;
+											$Perguruan   		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[4]/td[3]",0)->plaintext;
+											$Program    		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[5]/td[3]",0)->plaintext;
+											$Nomor     			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[6]/td[3]",0)->plaintext;
+											$Semester			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[7]/td[3]",0)->plaintext;
+											$Status_Awal 		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[8]/td[3]",0)->plaintext;
+											$Status_Mahasiswa	= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[9]/td[3]",0)->plaintext;	
+											
+					scraperwiki::save_sqlite(array('name'), array('name' => $Nomor,
+                                             'Nama' => $Nama,
+                                             'Jenis' => $Jenis, 
+                                             'Perguruan' => $Perguruan, 
+                                             'Program' => $Program, 
+                                             'Semester' => $Semester, 
+                                             'Status_Awal' => $Status_Awal, 
+                                             'Status_Mahasiswa' => $Status_Mahasiswa));	
+											}
+										}
 									}
 							}
 						}
