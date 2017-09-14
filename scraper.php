bba1d2c650ce8acdaabe1abfe5645b9e4e443599
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-14 14:39:30 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,7 +10,6 @@ for($i = 0; $i < count($links); $i++)
 				foreach($link->find("//[@id='mahasiswa']/table/tbody/tr") as $element)
 					{
 						$totalcountofstudenteachsemester	= $element->find("td[3]/a" , 0)->plaintext;
-						
 						$number = $totalcountofstudenteachsemester / 20;
 						$Pages =(int)$number;
 						$student 	= $element->find("td[3]/a" , 0)->href;
@@ -37,33 +36,53 @@ for($i = 0; $i < count($links); $i++)
 												$URL = $data[$loopo];
 												$Pagestudent = file_get_html($URL);
 												
-									if($Pagestudent)
-									{
-									//This is Details of Students.
-									$Nama 			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[1]/td[3]",0)->plaintext;
-									$Jenis  		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[2]/td[3]",0)->plaintext;
-									$Perguruan   		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[4]/td[3]",0)->plaintext;
-									$Program    		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[5]/td[3]",0)->plaintext;
-									echo $Nomor     		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[6]/td[3]",0)->plaintext;
-									echo "<br/>";
-									$Semester		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[7]/td[3]",0)->plaintext;
-									$Status_Awal 		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[8]/td[3]",0)->plaintext;
-									$Status_Mahasiswa	= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[9]/td[3]",0)->plaintext;												
-									
-									scraperwiki::save_sqlite(array('name'), 
-    array('name' => $Nomor, 
-          'Nama' => $Nama, 
-          'Jenis' => $Jenis,
-          'Perguruan' => $Perguruan,
-          'Semester' => $Semester,
-          'Status_Awal' => $Status_Awal,
-          'Status_Mahasiswa' => $Status_Mahasiswa
+											if($Pagestudent)
+											{
+											//This is Details of Students.
+											$Nama 				= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[1]/td[3]",0)->plaintext;
+											$Jenis  			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[2]/td[3]",0)->plaintext;
+											$Perguruan   		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[4]/td[3]",0)->plaintext;
+											$Program    		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[5]/td[3]",0)->plaintext;
+											$Nomor     			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[6]/td[3]",0)->plaintext;
+											$Semester			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[7]/td[3]",0)->plaintext;
+											$Status_Awal 		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[8]/td[3]",0)->plaintext;
+											$Status_Mahasiswa	= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[9]/td[3]",0)->plaintext;												
+											
+
+											$servername = "localhost";
+											$username = "root";
+											$password = "";
+											$dbname = "indonesia";
+
+											// Create connection
+											$conn = new mysqli($servername, $username, $password, $dbname);
+											// Check connection
+											if ($conn->connect_error) 
+											{
+												die("Connection failed: " . $conn->connect_error);
+											} 
+
+											$sql = "INSERT INTO forlap (Pagestudent ,Nama, Jenis , Perguruan , Program , Nomor , Semester , Status_Awal , Status_Mahasiswa)
+											VALUES ('$URL ','$Nama', '$Jenis' , '$Perguruan' , '$Program' , '$Nomor' , '$Semester' , '$Status_Awal' , '$Status_Mahasiswa')";
+
+											if ($conn->query($sql) === TRUE) 
+											{
+												echo 'Added';
+												echo '<br/>';
+											} else 
+											{
+												echo 'Error: ' . $sql . '<br>' . $conn->error;
+											}
+
+											$conn->close();			
+								
           
-    ));
+
 			
-									}
+										
 									
-										}
+								
+										}								
 
 									}
 								}
@@ -77,6 +96,8 @@ for($i = 0; $i < count($links); $i++)
 						
 					}
 			}
+		
+		}
 	}
 
 ?>
