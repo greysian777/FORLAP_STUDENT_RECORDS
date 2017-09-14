fe7081c13d40136d1149368ff7a7be6247855a63
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-14 15:05:32 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,12 +1,11 @@
 <?
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-error_reporting(0);
 $links = array("https://forlap.ristekdikti.go.id/prodi/detail/MzM5RDRFRUUtODcwRC00QUJBLUI3REYtODU4REFBRkQ4OTRC");
 for($i = 0; $i < count($links); $i++)
 	{
 			$link = file_get_html($links[$i]);
-			if($link != null || $link == "")
+			if($link != null || $link != "")
 			{
 				foreach($link->find("//[@id='mahasiswa']/table/tbody/tr") as $element)
 					{
