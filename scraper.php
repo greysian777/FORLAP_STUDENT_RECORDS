2033cf601b7099c4a1eab6fb748e45ea04cd92a7
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-09-14 21:30:53 -0700

    Add files via upload

---
+++ scraper.php
@@ -0,0 +1,77 @@
+<?
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$links = array("https://forlap.ristekdikti.go.id/prodi/detail/OEJDQjE0QTYtMzE1Ri00RjY1LUJFQkItQTQ1QzlFMEIyREY1",
+"https://forlap.ristekdikti.go.id/prodi/detail/QjAwRkIwREUtMUVCOC00MEMwLTk1MDctQjQ3NzlGRUM5MzQ5",
+"https://forlap.ristekdikti.go.id/prodi/detail/MDc1MDUxREItNDE3Ri00NDc4LUEyODgtRTEwRkFFODQyRDE3",
+"https://forlap.ristekdikti.go.id/prodi/detail/RDdGQUEwRTgtQTIzNC00OTA0LUIzRjgtNUNFNDlBOTVFQkVE"
+);
+for($i = 0; $i < count($links); $i++)
+	{
+			$link = file_get_html($links[$i]);
+			if($link)
+				{
+			foreach($link->find("//[@id='mahasiswa']/table/tbody/tr") as $element)
+			{
+				$totalcountofstudenteachsemester	= 	$element->find("td[3]/a" , 0)->plaintext;
+				$number 				= 	$totalcountofstudenteachsemester / 20;
+				$Pages 					=   	(int)$number;
+				$student 				= 	$element->find("td[3]/a" , 0)->href;
+				
+				
+						for($loop = 0; $loop <= $totalcountofstudenteachsemester; $loop+=20)
+						{
+							$urls =  $student . "/". $loop;
+							if($urls !== "/0")
+							{
+								$DAKUMENTPAGE = file_get_html($urls);
+								foreach($DAKUMENTPAGE->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr") as $SARTOUT)
+									{
+										$SerNo 		= $SARTOUT->find("td", 0)->plaintext;
+										$NIM 		= $SARTOUT->find("td", 1)->plaintext;
+										$Name 		= $SARTOUT->find("td" , 2)->plaintext;
+										$Namehref 	= $SARTOUT->find("td/a" , 0)->href;
+									
+										if($Namehref != null || $Namehref != "")
+										{
+											$Pagestudent = file_get_html($Namehref);
+											
+											//This is Details of Students.
+											$Nama 				= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[1]/td[3]",0)->plaintext;
+											$Jenis  			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[2]/td[3]",0)->plaintext;
+											$Perguruan   			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[4]/td[3]",0)->plaintext;
+											$Program    			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[5]/td[3]",0)->plaintext;
+											$Nomor    			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[6]/td[3]",0)->plaintext;
+											$Semester			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[7]/td[3]",0)->plaintext;
+											$Status_Awal 			= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[8]/td[3]",0)->plaintext;
+											$Status_Mahasiswa		= $Pagestudent->find("/html/body/div[2]/div[2]/div[2]/div[1]/div/table/tbody/tr[9]/td[3]",0)->plaintext;												
+											
+											/* scraperwiki::save_sqlite(array('name'), array('name' => $info['Nomor'],
+											     'Nama' 		=> $info['Nama'],
+											     'Jenis' 		=> $info['Jenis'], 
+											     'Perguruan'	=> $info['Perguruan'], 
+											     'Program' 		=> $info['Program'], 
+											     'Semester' 	=> $info['Semester'], 
+											     'Status_Awal' 	=> $info['Status_Awal'], 
+											     'Status_Mahasiswa' => $info['Status_Mahasiswa']
+												));*/				     
+								  $record = array( 'num' =>$Nomor, 'name' => $Nama,'jenis' => $Jenis , 'perguruan' => $Perguruan , 'program' => $Program, 'semester' => $Semester, 'statusawal' => $Status_Awal , 'statusmahasiswa' => $Status_Mahasiswa, 'namehref' => $Namehref, 'link' => $links[$i]);
+            								scraperwiki::save(array('num','name','jenis','perguruan','program','semester','statusawal','statusmahasiswa','namehref','link'), $record); 							     
+										
+											
+									
+																		
+											 
+										}
+										
+										
+									}
+							}
+						}
+				
+				
+			}
+				}
+	}
+
+?>
